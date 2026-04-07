321bd6e07535aa36934d4c079022985cc36dbeb5
Souvik Ghosh <91154217+sou-vik@users.noreply.github.com>
2026-04-07 13:01:36 +0000

    second commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "souviksouvik4321@gmail.com"
+var accountPswd = "viksou"
+accountCity = "Kolkata";
+
+accountEmail = "sousi.gmail.com"
+accountPswd = "jali"
+accountCity = "Guwahati"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+// alert(3+3)           we are using nodejs, not browser
+
+//number => 2 to the power 53
+//bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined => 
+//symbol => unique
+
+console.log(typeof "hitesh");
